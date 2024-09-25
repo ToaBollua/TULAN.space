ff8781c472f4532372468821c7f0c2dade4a6448
ToaBollua <bollua@proton.me>
2024-09-25 15:37:26 -0300

    Lotta changes, fkit

--- App/models.py
+++ App/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TradeHub(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='img/')
+
+    def __str__(self):
+        return self.name
--- App/views.py
+++ App/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import TradeHub
 
 def index(request):
+    """Renders the index page."""
     return render(request, 'App/index.html')
 
 def gallery(request):
+    """Renders the gallery page."""
     return render(request, 'App/gallery.html')
+
+def trade_hubs_view(request):
+    trade_hubs = TradeHub.objects.all()
+    return render(request, 'App/trade_hubs.html', {'trade_hubs': trade_hubs})
